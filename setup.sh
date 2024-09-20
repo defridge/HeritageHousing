2aa602d45edb3d582a29bbcc7b5d329496963273
Luke Masterson <info@herosgymtslc.com>
2024-09-20 18:13:26 +0000

    Updated requirements.txt file with production packages only

--- setup.sh
+++ setup.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
-
-# Ensure correct requirements file is used
-echo "Installing dependencies from requirements_prod.txt..."
-pip install -r requirements_prod.txt
-
-# Create the Streamlit configuration directory
 mkdir -p ~/.streamlit/
-
-# Create the Streamlit config file
 echo "\
 [server]\n\
 headless = true\n\
@@ -15,5 +6,3 @@ port = $PORT\n\
 enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
-
-echo "Setup complete. Streamlit configuration written to ~/.streamlit/config.toml"
